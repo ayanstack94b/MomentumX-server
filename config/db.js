678fe5c61b335b3c1136c7cd25552d11c36cf1d4
unknown <ayondevs.94b@gmail.com>
2026-06-18 19:25:48 +0530

    setup better auth with express and mongodb adapter, configure auth instance, connect mongodb atlas, add auth endpoint handler, enable email password authentication and trusted origins

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = process.env.MONGODB_URI;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require("express");
 require("dotenv").config();
+
+const express = require("express");
 const cors = require("cors");
-const { client, connectDB } = require("./config/db");
+const { toNodeHandler } = require("better-auth/node");
+const { connectDB } = require("./config/db");
+const { auth } = require("./lib/auth");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,15 +19,12 @@ app.use(
 
 app.use(express.json());
 
-connectDB();
-
-
 // Routes
 app.get("/", (req, res) => {
   res.send("MomentumX Server Running");
 });
-
-
+app.use("/api/auth", toNodeHandler(auth));
+// app.all("/api/auth/:path(*)", toNodeHandler(auth));
 
 connectDB();
 // Server
