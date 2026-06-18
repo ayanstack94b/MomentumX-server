2c21e9104de0a47d0e0b8f0e631bf50416184ea1
unknown <ayondevs.94b@gmail.com>
2026-06-18 17:01:50 +0530

    setup backend foundation with express configuration, mongodb atlas connection, environment variables, project structure, better auth installation, auth configuration bootstrap, and database connectivity testing

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { client, connectDB };
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
-const app = express();
-const port = 5000;
+require("dotenv").config();
+const cors = require("cors");
+const { client, connectDB } = require("./config/db");
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+connectDB();
+
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("MomentumX Server Running");
 });
 
+
+
+connectDB();
+// Server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`MomentumX running on port ${port}`);
 });
