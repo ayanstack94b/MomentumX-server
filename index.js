fd6f2fbb72c09996d54bd369ead635507898b0bc
unknown <ayondevs.94b@gmail.com>
2026-06-24 23:39:28 +0530

    Configured production environment settings

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 //  Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
